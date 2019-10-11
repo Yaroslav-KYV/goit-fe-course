f83a5e7459c39b28775efd830326babd096ec6e5
Yaroslav <kovshun.yv@gmail.com>
2019-10-11 17:43:39 +0300

    add method bind to task-02

--- JS/homework-04/task-02.js
+++ JS/homework-04/task-02.js
@@ -1,5 +1,32 @@
 /*
 Исправь ошибки которые будут в консоли, чтобы скрипт заработал.
+
+const inventory = {
+  items: ['Монорельса', 'Фильтр'],
+  add(itemName) {
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    this.items = this.items.filter(item => item !== itemName);
+  },
+};
+
+
+
+const invokeInventoryAction = function(itemName, action) {
+  console.log(`Invoking ${action.name} opeartion on ${itemName}`);
+  action(itemName);
+};
+
+invokeInventoryAction('Аптечка', inventory.add);
+// Invoking add opeartion on Аптечка
+
+console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
+
+invokeInventoryAction('Фильтр', inventory.remove);
+// Invoking remove opeartion on Фильтр
+
+console.log(inventory.items); // ['Монорельса', 'Аптечка']
 */
 
 /*
@@ -12,25 +39,24 @@ const inventory = {
     this.items.push(itemName);
   },
   remove(itemName) {
-    this.items = this.items.filter(item => item !== itemName);
+    this.items = this.items.filter(item => item !== itemName); 
   },
 };
 
-const invokeInventoryAction = function(itemName, action) {
+ const invokeInventoryAction = function(itemName, action) {
   console.log(`Invoking ${action.name} operation on ${itemName}`);
-  if(action === inventory.add) {
-    return inventory.add(itemName);
-  } else {
-    return inventory.remove(itemName);
-  }
+  action(itemName);
+  // (action === inventory.add) ? inventory.add(itemName) : inventory.remove(itemName);
 };
 
-invokeInventoryAction('Аптечка', inventory.add);
+
+
+invokeInventoryAction('Аптечка', inventory.add.bind(inventory));
 // Invoking add operation on Аптечка
 
 console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
 
-invokeInventoryAction('Фильтр', inventory.remove);
+invokeInventoryAction('Фильтр', inventory.remove.bind(inventory));
 // Invoking remove operation on Фильтр
 
 console.log(inventory.items); // ['Монорельса', 'Аптечка']
